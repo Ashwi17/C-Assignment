c4bdd7f0faa92d0a00ab371b454293d30fdbb66f
ASHWINI <ashwrp14@gmail.com>
2024-08-31 20:55:05 +0530

    sumof5no.c
    C++ Program to print sum of 1st five natural numbers.

---
+++ sumof5no.c
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int sum=0;
+    cout<<"Enter the Number: 5\n";
+    for(int i=0;i<=5;i++){
+        sum+=i;
+    }
+    cout<<"The sum of five Number = "<<sum;
+    return 0;
+}
