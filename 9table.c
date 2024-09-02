fcda1868f6731e096756bd2b893b51856de2cd7e
ASHWINI <ashwrp14@gmail.com>
2024-09-02 21:34:20 +0530

    9table.c
    Print the 9 table from bottom up to 15.

---
+++ 9table.c
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num;
+    cout<<"Enter your Table Number: ";
+    cin>>num;
+    for(int i=0;i<=num;i++){
+        cout<<"9 x "<<i<<" = "<<9*i<<endl;
+    }
+    return 0;
+}
