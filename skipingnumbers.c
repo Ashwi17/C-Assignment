499d220ceca41546d0bfabbc042bc462a044d859
ASHWINI <ashwrp14@gmail.com>
2024-09-03 22:36:46 +0530

    skipingnumbers.c
    Get any random six numbers from the user, if any one the a particular number is multiple of 5 then skip the number and print all other data got from the user

--- skipingnumbers.c
+++ skipingnumbers.c
@@ -1,15 +1,15 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int i;
+    int num[6];
     cout<<"Enter the Numbers: \n";
-    for(i=1;i<=6;i++){
-       cin>>i;
-       if(i%5==0){
+    for(int i=1;i<=6;i++){
+       cin>>num[i];
+       if(num[i]%5==0){
            continue;
        }
        else{
-       cout<<"The numbers are not divisible by 5: "<<i<<endl;
+       cout<<"The numbers are not divisible by 5: "<<num[i]<<endl;
     }
     }
 }
